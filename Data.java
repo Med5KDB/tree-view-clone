abbaff0d2beb2810b04b286a7177a9478a6607e9
Med5KDB <kevindebruyne17med@gmail.com>
2024-05-30 20:14:36 +0000

    initial commit (add files archi)

---
+++ Data.java
@@ -0,0 +1,8 @@
+public abstract class Data {
+    public int size;
+    public String name;
+    public void display() {
+        System.out.println("Nom: " + name);
+        System.out.println("Taille: " + size);
+    }
+}
